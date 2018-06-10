d212ec570d10bfa6727e6eb87b8cb028e5fe9625
Sandeep Vattapparambil <sandeepv68@gmail.com>
2018-06-10 23:22:12 +0530

    updates

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
  */
 require('marko/node-require')
 const express = require('express');
+const createError = require('http-errors');
 const markoExpress = require('marko/express');
 const compression = require('compression');
 const lasso = require('lasso');
@@ -63,5 +64,33 @@ app.use(require('./middlewares'));
  */
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+/**
+ * catch 404 and forward to error handler
+ */
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+/**
+* error handler
+*/
+app.use(function (err, req, res, next) {
+  /**
+   * set locals, only providing error in development
+   */
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  let errorData = {
+      status: err.status || 500,
+      message: err.message,
+      stack: err.stack
+  };
+  /**
+   * render the error page
+   */
+  res.status(err.status || 500);
+  res.render('error', errorData);
+});
 
 module.exports = app;
--- middlewares/index.js
+++ middlewares/index.js
@@ -5,6 +5,6 @@ const router = express.Router();
  * Middleware to augment the response object with helpers
  */
 router.use(require('./response'));
-router.use(require('./error'));
+//router.use(require('./error'));
 
 module.exports = router;
